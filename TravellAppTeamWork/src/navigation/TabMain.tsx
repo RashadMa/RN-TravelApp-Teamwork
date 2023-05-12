b8237cfad95506a21e8b46003579f31f545fc1d2
Rashad <87971037+RashadM0@users.noreply.github.com>
2023-05-12 15:52:08 +0400

    Hehe

---
+++ TravellAppTeamWork/src/navigation/TabMain.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TabMain = () => {
+      return (
+            <View>
+                  <Text>TabMain</Text>
+            </View>
+      )
+}
+
+export default TabMain
+
+const styles = StyleSheet.create({})
---
+++ TravellAppTeamWork/src/network/api.ts
@@ -0,0 +1,39 @@
+import {axiosInstance} from './axiosInstance';
+
+export class BaseNetwork {
+  async getAll(url: string) {
+    let response: any[] = [];
+
+    try {
+      await axiosInstance.get(url).then(res => {
+        response = res.data;
+      });
+    } catch (error) {
+      throw error;
+    }
+
+    return response;
+  }
+
+  async add(url: string, data: any) {
+    let response = {};
+
+    await axiosInstance.post(url, data).then(res => {
+      console.log('DATA', res.data);
+
+      response = res.data;
+    });
+
+    return response;
+  }
+
+  async getById(url: string, id: number) {
+    let response = {};
+
+    await axiosInstance.get(url + id).then(res => {
+      response = res.data;
+    });
+
+    return response;
+  }
+}
