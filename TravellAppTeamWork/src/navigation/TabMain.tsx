a669beafd29ca143d4699fbdc62930cbd379b69b
Rashad <87971037+RashadM0@users.noreply.github.com>
2023-05-13 15:27:16 +0400

    Tab Structure

--- TravellAppTeamWork/src/navigation/TabMain.tsx
+++ TravellAppTeamWork/src/navigation/TabMain.tsx
@@ -1,11 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Saved from '../screens/saved/Saved';
+import Search from '../screens/search/Search';
+import HomeStack from './stacks/home/HomeStack';
+
+const Tab = createBottomTabNavigator();
 
 const TabMain = () => {
       return (
-            <View>
-                  <Text>TabMain</Text>
-            </View>
+            <>
+                  <Tab.Navigator screenOptions={{ headerShown: false }}>
+                        <Tab.Screen
+                              name="homestack"
+                              component={HomeStack}
+                              options={{
+
+                              }}
+                        />
+                        <Tab.Screen
+                              name="search"
+                              component={Search}
+                              options={{
+
+                              }}
+                        />
+                        <Tab.Screen
+                              name="saved"
+                              component={Saved}
+                              options={{
+                              }}
+                        />
+                  </Tab.Navigator>
+            </>
       )
 }
 
---
+++ TravellAppTeamWork/src/screens/home/Homescreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Homescreen = () => {
+  return (
+    <View>
+      <Text>Homescreen</Text>
+    </View>
+  )
+}
+
+export default Homescreen
+
+const styles = StyleSheet.create({})
