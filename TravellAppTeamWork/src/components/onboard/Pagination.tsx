d02405e7b064d1701a2b5cdf4f5a23e3ccbc4108
amilgasimov <93605485+amilgasimov@users.noreply.github.com>
2023-05-13 16:43:25 +0400

    Test2

--- TravellAppTeamWork/src/components/onboard/Pagination.tsx
+++ TravellAppTeamWork/src/components/onboard/Pagination.tsx
@@ -6,9 +6,9 @@ const Pagination = ({ datas, currentIndex }: any) => {
       return (
             <View style={styles.container}>
                   {
-                        datas.map((e: any, i: any) => (
-                              <View key={e.id} style={[styles.dot, currentIndex + 1 <= i ? { opacity: 0.5, } : { opacity: 1 }]} />
-                        ))
+                        // datas.map((e: any, i: any) => (
+                        //       <View key={e.id} style={[styles.dot, currentIndex + 1 <= i ? { opacity: 0.5, } : { opacity: 1 }]} />
+                        // ))
                   }
             </View>
       )
