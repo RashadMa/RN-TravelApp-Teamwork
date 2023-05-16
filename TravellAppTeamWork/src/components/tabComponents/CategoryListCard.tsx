9b96e255e00110cb8c988ea11e9f5a259ee2d3c9
faridjavadov <faridjavadov@outlook.com>
2023-05-16 21:51:54 +0400

    Search by Category fixed

--- TravellAppTeamWork/src/components/tabComponents/CategoryListCard.tsx
+++ TravellAppTeamWork/src/components/tabComponents/CategoryListCard.tsx
@@ -8,11 +8,11 @@ const CategoryListCard = (props: any) => {
   }
   return (
     <View>
-      <TouchableOpacity onPress={SearchByCategory}>
-        <Text style={{ backgroundColor: isPressed ? '#E0783E' : '#1C1C1C', color: 'white', fontSize: 14, padding: 10, borderWidth: 0.5, borderRadius: 8, marginRight: 10, borderColor: "#262626" }}>
+      
+        <Text style={{ backgroundColor: props.color, color: 'white', fontSize: 14, padding: 10, borderWidth: 0.5, borderRadius: 8, marginRight: 10, borderColor: "#262626" }}>
           {props.item.icon}  {props.item.name}
         </Text>
-      </TouchableOpacity>
+     
     </View>
   )
 }
